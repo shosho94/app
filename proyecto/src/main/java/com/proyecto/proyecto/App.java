9676fbd3ffddceb1592017123b4a7dca8e5d75b9
ROBERTHUTN <30377015+ROBERTHUTN@users.noreply.github.com>
2019-02-15 09:50:06 -0500

    proyecto maven

---
+++ proyecto/src/main/java/com/proyecto/proyecto/App.java
@@ -0,0 +1,16 @@
+package com.proyecto.proyecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "UNIVERSIDAD TÉCNICA DEL NORTE" );
+        System.out.println( "         FICA-CISIC" );
+        System.out.println( "Integreantes:Beltrán, Bastidas, Pinchao" );
+       
+    }
+}
